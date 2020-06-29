1c7ab3fbdac87d8db424f5742f409f4874fc5de9
Minh-Tri Pham <pmtri80@gmail.com>
2020-06-29 16:34:18 +0100

    docs for v0.2.0

---
+++ docs/install.rst
@@ -0,0 +1,12 @@
+Installation
+============
+
+Install via pip::
+
+    pip3 install mtpandas
+
+Build from source::
+
+    git clone https://github.com/inteplus/mtpandas.git
+    cd mtpandas
+    python3 setup.py install
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_namespace_packages
+from mt.pandas.version import VERSION
+
+setup(
+    name='mtpandas',
+    version=VERSION,
+    description="MT's extra modules for pandas.",
+    author=["Minh-Tri Pham"],
+    packages=find_namespace_packages(include=['mt.*']),
+    install_requires=[
+        'pandas>=0.23.0',  # for dataframes, and we need custom dtypes
+        'dask[dataframe]',  # for reading chunks of CSV files in parallel
+        'mtbase>=0.4.0',  # Minh-Tri's base modules for logging and multi-threading
+    ],
+)
