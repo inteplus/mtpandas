2bf72f241ab5dbcdaf95320036b5002486cc4e50
Minh-Tri Pham <minhtri.pham@winnowsolutions.com>
2021-05-19 23:26:44 +0100

    v0.4.6: nicer progress printout

--- mt/pandas/csv.py
+++ mt/pandas/csv.py
@@ -82,59 +82,64 @@ def read_csv(path, show_progress=False, **kwargs):
 
         spinner = Halo(text="dfloading '{}'".format(path), spinner='dots') if show_progress else dummy_scope
         with spinner:
-            # extract 'index_col' and 'dtype' from kwargs
-            index_col = kwargs.pop('index_col', None)
-            dtype = kwargs.pop('dtype', None)
+            try:
+                # extract 'index_col' and 'dtype' from kwargs
+                index_col = kwargs.pop('index_col', None)
+                dtype = kwargs.pop('dtype', None)
 
-            # load the metadata
-            if isinstance(spinner, Halo):
-                spinner.text = "loading the metadata"
-            meta = None if fp2 is None else json.load(fp2)
+                # load the metadata
+                if show_progress:
+                    spinner.text = "loading the metadata"
+                meta = None if fp2 is None else json.load(fp2)
 
-            if meta is None:
-                if isinstance(spinner, Halo):
-                    spinner.succeed("dfloaded '{}' with no metadata".format(path))
-                return df
+                if meta is None:
+                    if show_progress:
+                        spinner.succeed("dfloaded '{}' with no metadata".format(path))
+                    return df
 
-            # From now on, meta takes priority over dtype. We will ignore dtype.
-            kwargs['dtype'] = 'object'
+                # From now on, meta takes priority over dtype. We will ignore dtype.
+                kwargs['dtype'] = 'object'
 
-            # update index_col if it does not exist and meta has it
-            if index_col is None and len(meta['index_names']) > 0:
-                index_col = meta['index_names']
+                # update index_col if it does not exist and meta has it
+                if index_col is None and len(meta['index_names']) > 0:
+                    index_col = meta['index_names']
 
-            # adjust the returning dataframe based on the given meta
-            s = meta['columns']
-            for x in s:
-                if isinstance(spinner, Halo):
-                    spinner.text = "checking column '{}'".format(x)
-                y = s[x]
-                if y == 'datetime64[ns]':
-                    df[x] = _pd.to_datetime(df[x])
-                elif isinstance(y, list) and y[0] == 'category':
-                    cat_dtype = _pd.api.types.CategoricalDtype(categories=y[1], ordered=y[2])
-                    df[x] = df[x].astype(cat_dtype)
-                elif y == 'int64':
-                    df[x] = df[x].astype(_np.int64)
-                elif y == 'uint8':
-                    df[x] = df[x].astype(_np.uint8)
-                elif y == 'float64':
-                    df[x] = df[x].astype(_np.float64)
-                elif y == 'bool':
-                    # dd is very strict at reading a csv. It may read True as 'True' and False as 'False'.
-                    df[x] = df[x].replace('True', True).replace(
-                        'False', False).astype(_np.bool)
-                elif y == 'object':
-                    pass
-                else:
-                    raise OSError("Unknown dtype for conversion {}".format(y))
+                # adjust the returning dataframe based on the given meta
+                s = meta['columns']
+                for x in s:
+                    if show_progress:
+                        spinner.text = "checking column '{}'".format(x)
+                    y = s[x]
+                    if y == 'datetime64[ns]':
+                        df[x] = _pd.to_datetime(df[x])
+                    elif isinstance(y, list) and y[0] == 'category':
+                        cat_dtype = _pd.api.types.CategoricalDtype(categories=y[1], ordered=y[2])
+                        df[x] = df[x].astype(cat_dtype)
+                    elif y == 'int64':
+                        df[x] = df[x].astype(_np.int64)
+                    elif y == 'uint8':
+                        df[x] = df[x].astype(_np.uint8)
+                    elif y == 'float64':
+                        df[x] = df[x].astype(_np.float64)
+                    elif y == 'bool':
+                        # dd is very strict at reading a csv. It may read True as 'True' and False as 'False'.
+                        df[x] = df[x].replace('True', True).replace(
+                            'False', False).astype(_np.bool)
+                    elif y == 'object':
+                        pass
+                    else:
+                        raise OSError("Unknown dtype for conversion {}".format(y))
 
-            # set the index_col if it exists
-            if index_col is not None and len(index_col) > 0:
-                df = df.set_index(index_col, drop=True)
+                # set the index_col if it exists
+                if index_col is not None and len(index_col) > 0:
+                    df = df.set_index(index_col, drop=True)
 
-            if isinstance(spinner, Halo):
-                spinner.succeed("dfloaded '{}'".format(path))
+                if show_progress:
+                    spinner.succeed("dfloaded '{}'".format(path))
+            except:
+                if show_progress:
+                    spinner.succeed("failed to dfload '{}'".format(path))
+                raise
 
         return df
 
@@ -159,76 +164,81 @@ def read_csv(path, show_progress=False, **kwargs):
                 fp2 = None
             return process(path, fp1, fp2, show_progress=show_progress, **kwargs)
 
-
 read_csv.__doc__ = '''Read a CSV file or a CSV-zipped file into a pandas.DataFrame, passing all arguments to :func:`pandas.read_csv`. Keyword argument 'show_progress' tells whether to show progress in the terminal.''' + _pd.read_csv.__doc__
 
 
 def to_csv(df, path, index='auto', file_mode=0o664, show_progress=False, **kwargs):
 
-    if show_progress:
-        bar = tqdm(total=3, unit='step')
+    spinner = Halo(text="dfsaving '{}'".format(path), spinner='dots') if show_progress else dummy_scope
+    with spinner:
+        try:
+            if index=='auto':
+                index = bool(df.index.name)
 
-    if index=='auto':
-        index = bool(df.index.name)
+            # make sure we do not concurrenly access the file
+            with _p.lock(path, to_write=True):
+                if path.lower().endswith('.csv.zip'):
+                    # write the csv file
+                    path2 = path+'.tmp.zip'
+                    dirpath = _p.dirname(path)
+                    if dirpath:
+                        _p.make_dirs(dirpath)
+                    if not _p.exists(dirpath):
+                        _t.sleep(1)
 
-    # make sure we do not concurrenly access the file
-    with _p.lock(path, to_write=True):
-        if path.lower().endswith('.csv.zip'):
-            # write the csv file
-            path2 = path+'.tmp.zip'
-            dirpath = _p.dirname(path)
-            if dirpath:
-                _p.make_dirs(dirpath)
-            if not _p.exists(dirpath):
-                _t.sleep(1)
+                    with _zf.ZipFile(path2, mode='w') as myzip:
+                        filename = _p.basename(path)[:-4]
+                        with myzip.open(filename, mode='w', force_zip64=True) as f: # csv
+                            data = df.to_csv(None, index=index, quoting=_csv.QUOTE_NONNUMERIC, **kwargs)
+                            f.write(data.encode())
+                        if show_progress:
+                            spinner.text = "saved CSV content"
+                        with myzip.open(filename[:-4]+'.meta', mode='w') as f: # meta
+                            data = json.dumps(metadata(df))
+                            f.write(data.encode())
+                        if show_progress:
+                            spinner.text = "saved metadata"
+                        res = None
+                    if file_mode:  # chmod
+                        _p.chmod(path2, file_mode)
+                else:
+                    # write the csv file
+                    path2 = path+'.tmp.csv'
+                    dirpath = _p.dirname(path)
+                    if dirpath:
+                        _p.make_dirs(dirpath)
+                    if not _p.exists(dirpath):
+                        _t.sleep(1)
+                    res = df.to_csv(path2, index=index, quoting=_csv.QUOTE_NONNUMERIC, **kwargs)
+                    if file_mode:  # chmod
+                        _p.chmod(path2, file_mode)
+                    if show_progress:
+                        spinner.text = "saved CSV content"
 
-            with _zf.ZipFile(path2, mode='w') as myzip:
-                filename = _p.basename(path)[:-4]
-                with myzip.open(filename, mode='w', force_zip64=True) as f: # csv
-                    data = df.to_csv(None, index=index, quoting=_csv.QUOTE_NONNUMERIC, **kwargs)
-                    f.write(data.encode())
-                if show_progress:
-                    bar.update()
-                with myzip.open(filename[:-4]+'.meta', mode='w') as f: # meta
-                    data = json.dumps(metadata(df))
-                    f.write(data.encode())
-                if show_progress:
-                    bar.update()
-                res = None
-            if file_mode:  # chmod
-                _p.chmod(path2, file_mode)
-        else:
-            # write the csv file
-            path2 = path+'.tmp.csv'
-            dirpath = _p.dirname(path)
-            if dirpath:
-                _p.make_dirs(dirpath)
-            if not _p.exists(dirpath):
-                _t.sleep(1)
-            res = df.to_csv(path2, index=index, quoting=_csv.QUOTE_NONNUMERIC, **kwargs)
-            if file_mode:  # chmod
-                _p.chmod(path2, file_mode)
-            if show_progress:
-                bar.update()
+                    # write the meta file
+                    path3 = path[:-4]+'.meta'
+                    json.dump(metadata(df), open(path3, 'wt'))
+                    try:
+                        if file_mode:  # chmod
+                            _p.chmod(path3, file_mode)
+                    except PermissionError:
+                        pass # for now
+                    if show_progress:
+                        spinner.text = "saved metadata"
 
-            # write the meta file
-            path3 = path[:-4]+'.meta'
-            json.dump(metadata(df), open(path3, 'wt'))
-            try:
-                if file_mode:  # chmod
-                    _p.chmod(path3, file_mode)
-            except PermissionError:
-                pass # for now
-            if show_progress:
-                bar.update()
+                _p.remove(path)
+                if _p.exists(path) or not _p.exists(path2):
+                    _t.sleep(1)
+                _p.rename(path2, path)
 
-        _p.remove(path)
-        if _p.exists(path) or not _p.exists(path2):
-            _t.sleep(1)
-        _p.rename(path2, path)
-        if show_progress:
-            bar.update()
-            bar.close()
-        return res
+                if isinstance(spinner, Halo):
+                    spinner.succeed("dfsaved '{}'".format(path))
+
+                return res
+
+        except:
+            if isinstance(spinner, Halo):
+                spinner.succeed("failed to dfsave '{}'".format(path))
+            raise
 
 to_csv.__doc__ = '''Write DataFrame to a comma-separated values (.csv) file or a CSV-zipped (.csv.zip) file. If keyword 'index' is 'auto' (default), the index column is written if and only if it has a name. Keyword 'file_mode' specifies the file mode when writing (passed directly to os.chmod if not None), and the remaining arguments and keywords are passed directly to :func:`DataFrame.to_csv`. Keyword argument 'show_progress' tells whether to show progress in the terminal.\n''' + _pd.DataFrame.to_csv.__doc__
