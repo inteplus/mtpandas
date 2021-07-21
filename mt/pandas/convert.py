f286778fe25842880a469d0385362730963387c5
Minh-Tri Pham <minhtri.pham@winnowsolutions.com>
2021-07-21 12:32:33 +0100

    v0.5.0: introducing dfpack and dfunpack which currently deal with ndarray and cv.Image columns only -- potentially breaking backward compatibility when serializing ndarray columns

--- mt/pandas/convert.py
+++ mt/pandas/convert.py
@@ -1,20 +1,133 @@
-import numpy as _np
-import pandas as _pd
+import json
+import pandas as pd
 from halo import Halo
+
+from mt import np, cv
 from mt.base.with_utils import dummy_scope
 import mt.base.path as _p
 from .csv import read_csv, to_csv
 
 
-__all__ = ['dfload', 'dfsave']
+__all__ = ['dfload', 'dfsave', 'dfpack', 'dfunpack']
 
 
 def array2list(x):
     '''Converts a nested numpy.ndarray object into a nested list object.'''
-    return [array2list(y) for y in x] if isinstance(x, _np.ndarray) and x.ndim == 1 else x
+    return [array2list(y) for y in x] if isinstance(x, np.ndarray) and x.ndim == 1 else x
 
 
-def dfload(df_filepath, *args, show_progress=False, parquet_convert_ndarray_to_list=False, **kwargs):
+def dfpack(df, spinner=None):
+    '''Packs a dataframe into a more compact format.
+
+    At the moment, it converts each ndarray column into 3 columns, and each cv.Image column into a json column.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        dataframe to be packed
+    spinner : Halo, optional
+        spinner for tracking purposes
+
+    Returns
+    -------
+    pandas.DataFrame
+        output dataframe
+    '''
+
+    df2 = df[[]].copy() # copy the index
+    for key in df.columns:
+        # determine whether a column is an ndarray column or an Image column or a normal column
+        if len(df[key]) == 0:
+            can_be_ndarray = False
+            can_be_Image = False
+        else:
+            can_be_ndarray = True
+            can_be_Image = True
+            for x in df[key].tolist():
+                if x is None:
+                    continue
+                if isinstance(x, np.ndarray):
+                    can_be_Image = False
+                    if not can_be_ndarray:
+                        break
+                    continue
+                if isinstance(x, cv.Image):
+                    can_be_ndarray = False
+                    if not can_be_Image:
+                        break
+                    continue
+                break
+
+        if can_be_ndarray:
+            if spinner is not None:
+                spinner.text = "packing ndarray field '{}'".format(key)
+            df2[key+'_df_nd_ravel'] = df[key].apply(lambda x: None if x is None else x.ravel())
+            df2[key+'_df_nd_shape'] = df[key].apply(lambda x: None if x is None else np.array(x.shape))
+            df2[key+'_df_nd_dtype'] = df[key].apply(lambda x: None if x is None else x.dtype.str)
+
+        elif can_be_Image:
+            if spinner is not None:
+                spinner.text = "packing Image field '{}'".format(key)
+            df2[key+'_df_imm'] = df[key].apply(lambda x: None if x is None else json.dumps(x.to_json()))
+
+        else:
+            if spinner is not None:
+                spinner.text = "passing field '{}'".format(key)
+            df2[key] = df[key]
+
+    return df2
+
+
+def dfunpack(df, spinner=None):
+    '''Unpacks a compact dataframe into a more expanded format.
+
+    This is the reverse function of :func:`dfpack`.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        dataframe to be unpacked
+    spinner : Halo, optional
+        spinner for tracking purposes
+
+    Returns
+    -------
+    pandas.DataFrame
+        output dataframe
+    '''
+
+    key2 = '' # just to trick pylint
+    df2 = df[[]].copy() # copy the index
+    for key in df.columns:
+        if key.endswith('_df_imm'):
+            key2 = key[:-7]
+            if spinner is not None:
+                spinner.text = "unpacking Image field '{}'".format(key2)
+            df2[key2] = df[key].apply(lambda x: None if x is None else cv.Image.from_json(json.loads(x)))
+        elif key.endswith('_df_nd_ravel'):
+            key2 = key[:-12]
+            if spinner is not None:
+                spinner.text = "unpacking ndarray field '{}'".format(key2)
+            def unpack_ndarray(row):
+                ravel = row[key2+'_df_nd_ravel']
+                dtype = np.dtype(row[key2+'_df_nd_dtype'])
+                if isinstance(ravel, np.ndarray): # already a 1D array?
+                    ravel = ravel.astype(dtype)
+                else: # list or something else?
+                    ravel = np.array(ravel, dtype=dtype)
+                return ravel.reshape(row[key2+'_df_nd_shape'])
+            df2[key2] = df.apply(unpack_ndarray, axis=1)
+        elif '_df_nd_' in key:
+            continue
+        else:
+            if spinner is not None:
+                spinner.text = "passing field '{}'".format(key)
+            df2[key] = df[key]
+
+    return df2
+
+
+def dfload(df_filepath, *args, show_progress=False, unpack=True, parquet_convert_ndarray_to_list=False, **kwargs):
     '''Loads a dataframe file based on the file's extension.
 
     Parameters
@@ -23,6 +136,8 @@ def dfload(df_filepath, *args, show_progress=False, parquet_convert_ndarray_to_l
         local path to an existing dataframe. The file extension is used to determine the file type.
     show_progress : bool
         show a progress spinner in the terminal
+    unpack : bool
+        whether or not to unpack the dataframe after loading
     parquet_convert_ndarray_to_list : bool
         whether or not to convert 1D ndarrays in the loaded parquet table into Python lists
     args : list
@@ -48,17 +163,25 @@ def dfload(df_filepath, *args, show_progress=False, parquet_convert_ndarray_to_l
     path = df_filepath.lower()
 
     if path.endswith('.parquet'):
-        spinner = Halo("dfloading '{}'".format(path), spinner='dots') if show_progress else dummy_scope
-        with spinner:
+        if show_progress:
+            spinner = Halo("dfloading '{}'".format(path), spinner='dots')
+            scope = spinner
+        else:
+            spinner = None
+            scope = dummy_scope
+        with scope:
             try:
-                df = _pd.read_parquet(df_filepath, *args, **kwargs)
+                df = pd.read_parquet(df_filepath, *args, **kwargs)
 
                 if parquet_convert_ndarray_to_list:
                     for x in df.columns:
                         if show_progress:
                             spinner.text = 'converting column: {}'.format(x)
-                        if df.dtypes[x] == _np.dtype('O'): # object
+                        if df.dtypes[x] == np.dtype('O'): # object
                             df[x] = df[x].apply(array2list) # because Parquet would save lists into nested numpy arrays which is not we expect yet.
+
+                if unpack:
+                    df = dfunpack(df, spinner=spinner)
 
                 if show_progress:
                     spinner.succeed("dfloaded '{}'".format(path))
@@ -71,12 +194,17 @@ def dfload(df_filepath, *args, show_progress=False, parquet_convert_ndarray_to_l
 
 
     if path.endswith('.csv') or path.endswith('.csv.zip'):
-        return read_csv(df_filepath, *args, show_progress=show_progress, **kwargs)
+        df = read_csv(df_filepath, *args, show_progress=show_progress, **kwargs)
+
+        if unpack:
+            df = dfunpack(df)
+
+        return df
 
     raise TypeError("Unknown file type: '{}'".format(df_filepath))
 
 
-def dfsave(df, df_filepath, file_mode=0o664, show_progress=False, **kwargs):
+def dfsave(df, df_filepath, file_mode=0o664, show_progress=False, pack=True, **kwargs):
     '''Saves a dataframe to a file based on the file's extension.
 
     Parameters
@@ -89,6 +217,8 @@ def dfsave(df, df_filepath, file_mode=0o664, show_progress=False, **kwargs):
         file mode to be set to using :func:`os.chmod`. If None is given, no setting of file mode will happen.
     show_progress : bool
         show a progress spinner in the terminal
+    pack : bool
+        whether or not to pack the dataframe before saving
     kwargs : dict
         dictionary of keyword arguments to pass to the corresponding writer
 
@@ -107,15 +237,23 @@ def dfsave(df, df_filepath, file_mode=0o664, show_progress=False, **kwargs):
         if file type is unknown or if the input is not a dataframe
     '''
 
-    if not isinstance(df, _pd.DataFrame):
+    if not isinstance(df, pd.DataFrame):
         raise TypeError("Input must be a pandas.DataFrame. Got '{}'.".format(type(df)))
 
     path = df_filepath.lower()
 
     if path.endswith('.parquet'):
-        spinner = Halo(text="dfsaving '{}'".format(path), spinner='dots') if show_progress else dummy_scope
-        with spinner:
+        if show_progress:
+            spinner = Halo(text="dfsaving '{}'".format(path), spinner='dots')
+            scope = spinner
+        else:
+            spinner = None
+            scope = dummy_scope
+        with scope:
             try:
+                if pack:
+                    df = dfpack(df, spinner=spinner)
+
                 if not 'use_deprecated_int96_timestamps' in kwargs:
                     kwargs = kwargs.copy()
                     kwargs['use_deprecated_int96_timestamps'] = True # to avoid exception pyarrow.lib.ArrowInvalid: Casting from timestamp[ns] to timestamp[ms] would lose data: XXXXXXX
@@ -132,6 +270,8 @@ def dfsave(df, df_filepath, file_mode=0o664, show_progress=False, **kwargs):
         return res
 
     if path.endswith('.csv') or path.endswith('.csv.zip'):
+        if pack:
+            df = dfpack(df)
         return to_csv(df, df_filepath, file_mode=file_mode, show_progress=show_progress, **kwargs)
 
     raise TypeError("Unknown file type: '{}'".format(df_filepath))
