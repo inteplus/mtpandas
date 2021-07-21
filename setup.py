6b7ca8b11e332b6be4e850f64cf18a1db55fe6e4
Minh-Tri Pham <minhtri.pham@winnowsolutions.com>
2021-07-21 18:24:36 +0100

    v0.5.2: minor bug fixes

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ setup(
         'halo', # for showing some spinners
         'pandas>=1.0.3',  # for dataframes, and we need custom dtypes
         #'pyarrow', # for converting to/from parquet. But TX2 doesn't need pyarrow.
-        'mtbase>=1.4.6',  # Minh-Tri's base modules for logging and multi-threading
-        'mtopencv>=0.2.4', # Minh-Tri's OpenCV wrapper so we can use cv.Image
+        'mtbase>=1.4.7',  # Minh-Tri's base modules for logging and multi-threading
+        'mtopencv>=0.2.6', # Minh-Tri's OpenCV wrapper so we can use cv.Image
         'tqdm', # for drawing progress bars
         'tabview', # for viewing spreadsheet-like files
     ],
