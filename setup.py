27164f96bfc4bd997c59e3e925af4bdff6be39f0
Minh-Tri Pham <pmtri80@gmail.com>
2022-09-21 00:31:43 +0100

    v1.11: minimal python now is 3.6

--- setup.py
+++ setup.py
@@ -5,36 +5,39 @@ from packaging import version as pv
 from mt.pandas.version import version
 
 import platform
+
 py_ver = platform.python_version()
 
-if pv.parse(py_ver) < pv.parse('3.7'):
-    dependencies = ['dataclasses']
+if pv.parse(py_ver) < pv.parse("3.7"):
+    dependencies = ["dataclasses"]
 else:
     dependencies = []
 
 setup(
-    name='mtpandas',
+    name="mtpandas",
     version=version,
     description="MT's extra modules for pandas.",
     author=["Minh-Tri Pham"],
-    packages=find_namespace_packages(include=['mt.*']),
-    install_requires=dependencies + [
-        'halo', # for showing some spinners
-        'pandas',  # for dataframes, and we need custom dtypes
+    packages=find_namespace_packages(include=["mt.*"]),
+    install_requires=dependencies
+    + [
+        "halo",  # for showing some spinners
+        "pandas>=1.2",  # for dataframes, and we need custom dtypes
         #'h5py>=3', # for pdh5 file format. Lazy import because TX2 may not need it.
         #'pyarrow', # for converting to/from parquet. But TX2 doesn't need pyarrow.
-        'mtbase>=2.11',  # Minh-Tri's base modules for logging and multi-threading
-        'mtopencv>=1.1', # Minh-Tri's OpenCV wrapper so we can use cv.Image
-        'tqdm', # for drawing progress bars
-        'tabview', # for viewing spreadsheet-like files
+        "mtbase>=3.0",  # Minh-Tri's base modules for logging and multi-threading
+        "mtopencv>=1.1",  # Minh-Tri's OpenCV wrapper so we can use cv.Image
+        "tqdm",  # for drawing progress bars
+        "tabview",  # for viewing spreadsheet-like files
     ],
+    python_requires=">=3.6",  # we still have to support TX1 modules
     scripts=[
-        'scripts/dfconvert',
-        'scripts/dfview',
+        "scripts/dfconvert",
+        "scripts/dfview",
     ],
-    url='https://github.com/inteplus/mtpandas',
+    url="https://github.com/inteplus/mtpandas",
     project_urls={
-        'Documentation': 'https://mtdoc.readthedocs.io/en/latest/mt.pandas/mt.pandas.html',
-        'Source Code': 'https://github.com/inteplus/mtpandas',
-    }
+        "Documentation": "https://mtdoc.readthedocs.io/en/latest/mt.pandas/mt.pandas.html",
+        "Source Code": "https://github.com/inteplus/mtpandas",
+    },
 )
