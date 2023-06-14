57a7c8085733a8f0bcd3af3ea11affd3055514a2
Minh-Tri Pham <pmtri80@gmail.com>
2023-06-14 19:07:51 +0100

    introducing pd.parallel_apply()

--- mt/pandas/dataframe.py
+++ mt/pandas/dataframe.py
@@ -1,15 +1,18 @@
-'''Additional utilities dealing with dataframes.'''
+"""Additional utilities dealing with dataframes."""
 
 
 import pandas as pd
 from tqdm import tqdm
+from pandas_parallel_apply import DataFrameParallel
+
+from mt import tp, logg, ctx
 
 
-__all__ = ['rename_column', 'row_apply', 'warn_duplicate_records']
+__all__ = ["rename_column", "row_apply", "parallel_apply", "warn_duplicate_records"]
 
 
-def rename_column(df: pd.DataFrame, old_column: str, new_column:str) -> bool:
-    '''Renames a column in a dataframe.
+def rename_column(df: pd.DataFrame, old_column: str, new_column: str) -> bool:
+    """Renames a column in a dataframe.
 
     Parameters
     ----------
@@ -24,7 +27,7 @@ def rename_column(df: pd.DataFrame, old_column: str, new_column:str) -> bool:
     -------
     bool
         whether or not the column has been renamed
-    '''
+    """
     if not old_column in df.columns:
         return False
 
@@ -34,8 +37,8 @@ def rename_column(df: pd.DataFrame, old_column: str, new_column:str) -> bool:
     return True
 
 
-def row_apply(df: pd.DataFrame, func, bar_unit='it') -> pd.DataFrame:
-    '''Applies a function on every row of a pandas.DataFrame, optionally with a progress bar.
+def row_apply(df: pd.DataFrame, func, bar_unit="it") -> pd.DataFrame:
+    """Applies a function on every row of a pandas.DataFrame, optionally with a progress bar.
 
     Parameters
     ----------
@@ -50,7 +53,7 @@ def row_apply(df: pd.DataFrame, func, bar_unit='it') -> pd.DataFrame:
     -------
     pandas.DataFrame
         output series by invoking `df.apply`. And a progress bar is shown if asked.
-    '''
+    """
 
     if bar_unit is None:
         return df.apply(func, axis=1)
@@ -66,12 +69,71 @@ def row_apply(df: pd.DataFrame, func, bar_unit='it') -> pd.DataFrame:
         return df.apply(func2, axis=1)
 
 
+def parallel_apply(
+    df: pd.DataFrame,
+    func,
+    axis: int = 1,
+    n_cores: int = -1,
+    parallelism: str = "multiprocess",
+    logger: tp.Optional[logg.IndentedLoggerAdapter] = None,
+    scoped_msg: tp.Optional[str] = None,
+) -> pd.Series:
+    """Parallel-applies a function on every row or column of a pandas.DataFrame, optionally with a progress bar.
+
+    The method wraps class:`pandas_parallel_apply.DataFrameParallel`. The default axis is on rows.
+    The progress bars are shown if and only if a logger is provided.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        a dataframe
+    func : function
+        a function to map each item of the series to something. It must be pickable for parallel
+        processing.
+    axis : {0,1}
+        axis of applying. 1 for rows (default). 0 for columns.
+    n_cores : int
+        number of CPUs to use. Passed as-is to :class:`pandas_parallel_apply.DataFrameParallel`.
+    parallelism : {'multithread', 'multiprocess'}
+        multi-threading or multi-processing. Passed as-is to
+        :class:`pandas_parallel_apply.DataFrameParallel`.
+    logger : mt.logg.IndentedLoggerAdapter, optional
+        logger for debugging purposes.
+    scoped_msg : str, optional
+        whether or not to scoped_info the progress bars. Only valid if a logger is provided
+
+    Returns
+    -------
+    pandas.DataFrame
+        output dataframe by invoking `df.apply`.
+
+    See Also
+    --------
+    pandas_parallel_apply.DataFrameParallel
+        the wrapped class for the parallel_apply purpose
+    """
+
+    if logger:
+        dp = DataFrameParallel(df, n_cores=n_cores, parallelism=parallelism, pbar=True)
+        if scoped_msg:
+            context = logger.scoped_info(scoped_msg)
+        else:
+            context = ctx.nullcontext()
+    else:
+        dp = DataFrameParallel(df, n_cores=n_cores, parallelism=parallelism, pbar=False)
+        context = ctx.nullcontext()
+
+    with context:
+        return dp.apply(func, axis)
+
+
 def warn_duplicate_records(
-        df: pd.DataFrame,
-        keys: list,
-        msg_format: str = "Detected {dup_cnt}/{rec_cnt} duplicate records.",
-        logger=None):
-    '''Warns of duplicate records in the dataframe based on a list of keys.
+    df: pd.DataFrame,
+    keys: list,
+    msg_format: str = "Detected {dup_cnt}/{rec_cnt} duplicate records.",
+    logger=None,
+):
+    """Warns of duplicate records in the dataframe based on a list of keys.
 
     Parameters
     ----------
@@ -83,7 +145,7 @@ def warn_duplicate_records(
         the message to be logged. Two keyword arguments will be provided 'rec_cnt' and 'dup_cnt'.
     logger : logging.Logger, optional
         logger to warn if duplicate records are detected
-    '''
+    """
     if not logger:
         return
 
@@ -92,4 +154,4 @@ def warn_duplicate_records(
         keys = [keys]
     cnt1 = len(df[keys].drop_duplicates())
     if cnt1 < cnt0:
-        logger.warning(msg_format.format(dup_cnt=cnt0-cnt1, rec_cnt=cnt0))
+        logger.warning(msg_format.format(dup_cnt=cnt0 - cnt1, rec_cnt=cnt0))
