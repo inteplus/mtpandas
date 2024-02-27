6e694ce5cb40ba8561ddf1fd8bf2d41cb7362681
Minh-Tri <minhtri.pham@winnowsolutions.com>
2024-02-27 15:35:49 +0000

    v1.16: supporting pyproject.toml

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
+import os
+import platform
 from setuptools import setup, find_namespace_packages
 from packaging import version as pv
-from mt.pandas.version import version
-
-import platform
 
 py_ver = platform.python_version()
 
@@ -13,9 +12,10 @@ if pv.parse(py_ver) < pv.parse("3.7"):
 else:
     dependencies = []
 
+VERSION_FILE = os.path.join(os.path.dirname(__file__), "VERSION.txt")
+
 setup(
     name="mtpandas",
-    version=version,
     description="MT's extra modules for pandas.",
     author=["Minh-Tri Pham"],
     packages=find_namespace_packages(include=["mt.*"]),
@@ -40,5 +40,14 @@ setup(
     project_urls={
         "Documentation": "https://mtdoc.readthedocs.io/en/latest/mt.pandas/mt.pandas.html",
         "Source Code": "https://github.com/inteplus/mtpandas",
+    },
+    setup_requires=["setuptools-git-versioning<2"],
+    setuptools_git_versioning={
+        "enabled": True,
+        "version_file": VERSION_FILE,
+        "count_commits_from_version_file": True,
+        "template": "{tag}",
+        "dev_template": "{tag}.dev{ccount}+{branch}",
+        "dirty_template": "{tag}.post{ccount}",
     },
 )
